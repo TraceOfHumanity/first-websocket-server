375bdd502c7f50717f9a8375b5a1bd87c244255f
Alexander_Ovchar <azalexxx02@gmail.com>
2024-04-10 18:42:58 +0300

    add sass

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
-import "./index.css";
+import "./index.scss";
 import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
